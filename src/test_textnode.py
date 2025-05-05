70e2257fc940dc73d791994f09947b624983eb95
bonskpy <bonskpy@gmail.com>
2025-05-05 21:42:29 +0200

    add: text to node feature that parses markdown syntax and preparex text nodes for HTML conversion + tests

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -8,6 +8,7 @@ from textnode import (
     split_nodes_image,
     split_nodes_link,
     text_node_to_html_node,
+    text_to_textnodes,
 )
 
 
@@ -241,6 +242,35 @@ class TestSplitNodesImage(unittest.TestCase):
             ],
             new_nodes,
         )
+
+
+class TextTextToMarkdown(unittest.TestCase):
+    def test_all_types(self):
+        markdown_text = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)."
+        expected = [
+            TextNode("This is ", TextType.NORMAL, None),
+            TextNode("text", TextType.BOLD, None),
+            TextNode(" with an ", TextType.NORMAL, None),
+            TextNode("italic", TextType.ITALIC, None),
+            TextNode(" word and a ", TextType.NORMAL, None),
+            TextNode("code block", TextType.CODE, None),
+            TextNode(" and an ", TextType.NORMAL, None),
+            TextNode("obi wan image", TextType.IMG, "https://i.imgur.com/fJRm4Vk.jpeg"),
+            TextNode(" and a ", TextType.NORMAL, None),
+            TextNode("link", TextType.LINK, "https://boot.dev"),
+            TextNode(".", TextType.NORMAL, None),
+        ]
+        self.assertEqual(text_to_textnodes(markdown_text), expected)
+
+    def test_white_space(self):
+        markdown_text = " "
+        expected = [TextNode(" ", TextType.NORMAL, None)]
+        self.assertEqual(text_to_textnodes(markdown_text), expected)
+
+    def test_empty_string(self):
+        markdown_text = ""
+        expected = []
+        self.assertEqual(text_to_textnodes(markdown_text), expected)
 
 
 if __name__ == "__main__":
--- src/textnode.py
+++ src/textnode.py
@@ -112,43 +112,79 @@ def split_nodes_delimiter(
 
 
 def split_nodes_image(old_nodes: list[TextNode]) -> list[TextNode]:
+    """Split each TextNode so that images are extracted to separate TextNode"""
     result = []
     img_pattern = re.compile(r"(!\[[^\[\]]*\]\([^\(\)]*\))")  # link pattern
 
     for node in old_nodes:
-        node_parts = img_pattern.split(node.text)
-        print(node_parts)
+        # extraction should happen from TextType.NORMAL only
+        # this might change later if nesting will be implemented
+        if node.text_type != TextType.NORMAL:
+            result.append(node)
+        elif node.text_type == TextType.NORMAL:
+            node_parts = img_pattern.split(node.text)
 
-        for part in node_parts:
-            # do not add an empty node to the result list
-            if not part:
-                continue
-            if img_pattern.match(part):
-                # extract markdown returns a list of tuples
-                anchor, url = extract.extract_markdown_images(part)[0]
-                result.append(TextNode(anchor, TextType.IMG, url))
-            else:
-                result.append(TextNode(text=part, text_type=TextType.NORMAL))
+            for part in node_parts:
+                # do not add an empty node to the result list
+                if not part:
+                    continue
+                if img_pattern.match(part):
+                    # extract markdown returns a list of tuples
+                    anchor, url = extract.extract_markdown_images(part)[0]
+                    result.append(TextNode(anchor, TextType.IMG, url))
+                else:
+                    result.append(TextNode(text=part, text_type=TextType.NORMAL))
 
     return result
 
 
 def split_nodes_link(old_nodes: list[TextNode]) -> list[TextNode]:
+    """Split each TextNode so that links are extracted to separate TextNode"""
     result = []
     link_pattern = re.compile(r"(\[[^\]]+]\([^)]+\))")  # link pattern
 
     for node in old_nodes:
-        node_parts = link_pattern.split(node.text)
+        # extraction should happen from TextType.NORMAL only
+        # this might change later if nesting will be implemented
+        if node.text_type != TextType.NORMAL:
+            result.append(node)
+        elif node.text_type == TextType.NORMAL:
+            node_parts = link_pattern.split(node.text)
 
-        for part in node_parts:
-            # do not add an empty node to the result list
-            if not part:
-                continue
-            if link_pattern.match(part):
-                # extract markdown returns a list of tuples
-                label, url = extract.extract_markdown_links(part)[0]
-                result.append(TextNode(label, TextType.LINK, url))
-            else:
-                result.append(TextNode(text=part, text_type=TextType.NORMAL))
+            for part in node_parts:
+                # do not add an empty node to the result list
+                if not part:
+                    continue
+                if link_pattern.match(part):
+                    # extract markdown returns a list of tuples
+                    label, url = extract.extract_markdown_links(part)[0]
+                    result.append(TextNode(label, TextType.LINK, url))
+                else:
+                    result.append(TextNode(text=part, text_type=TextType.NORMAL))
 
     return result
+
+
+def text_to_textnodes(text: str) -> list[TextNode]:
+    """
+    Converts text to TextNodes which parse markdown syntax and prepare for HTML conversion
+
+    param text: str: markdown string to be coverted to HTML
+    return list[TextNode]: TextNode class used for markdown syntax parsing
+    """
+    # creating TextNode for syntax processing
+    text_node = TextNode(text=text, text_type=TextType.NORMAL)
+    # preparing delimiter config
+    # config part might be moved later in order to make code more reusable and maintainable
+    delimiter_config = {"**": TextType.BOLD, "_": TextType.ITALIC, "`": TextType.CODE}
+
+    processed_nodes = [text_node]
+    for delimiter, text_type in delimiter_config.items():
+        processed_nodes = split_nodes_delimiter(
+            processed_nodes, delimiter=delimiter, text_type=text_type
+        )
+
+    processed_nodes = split_nodes_image(processed_nodes)
+    processed_nodes = split_nodes_link(processed_nodes)
+
+    return processed_nodes
